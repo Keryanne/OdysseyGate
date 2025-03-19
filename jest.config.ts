fa63cf8755800863b9690550a3642a08fa9c1baf
Keryanne <47926567+Keryanne@users.noreply.github.com>
2025-03-19 12:20:31 +0100

    Fix test error

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  testMatch: ['**/+(*.)+(spec).+(ts)?(x)'],
+};
