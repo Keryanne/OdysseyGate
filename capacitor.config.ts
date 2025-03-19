250dc49f8aed0cff8f69d495525e91eff6546390
Keryanne <47926567+Keryanne@users.noreply.github.com>
2025-03-19 11:15:01 +0100

    Workflow issue - fix eslint

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'sky-odyssey-front',
+  appName: 'odyssey-gate',
   webDir: 'www'
 };
 
