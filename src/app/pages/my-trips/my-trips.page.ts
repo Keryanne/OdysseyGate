0611564b0af76ba05786b0ad81ddd66b64caaad6
Keryanne <47926567+Keryanne@users.noreply.github.com>
2025-03-20 16:33:54 +0100

    Feat(OG-0301): Add trips list

---
+++ src/app/pages/my-trips/my-trips.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { trips } from './mock-trips';
+import { PixabayService } from 'src/app/services/pixabay.service';
+
+@Component({
+  selector: 'app-my-trips',
+  templateUrl: './my-trips.page.html',
+  styleUrls: ['./my-trips.page.scss'],
+})
+export class MyTripsPage implements OnInit {
+  trips = trips;
+  images: { [key: string]: string } = {};
+
+  constructor( private pixabayService: PixabayService) { }
+
+  ngOnInit() {
+    this.loadImages();
+  }
+
+  loadImages() {
+    this.trips.forEach(trip => {
+      this.pixabayService.getImages(trip.city).subscribe(response => {
+        if (response.hits.length > 0) {
+          this.images[trip.city] = response.hits[0].webformatURL; // Prend la première image trouvée
+        } else {
+          this.images[trip.city] = 'assets/no-image.png'; // Image par défaut
+        }
+      });
+    });
+  }
+
+}
