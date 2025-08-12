e2f8743b1ebd72b94fbf903052c6af7ff8547ce4
Keryanne <47926567+Keryanne@users.noreply.github.com>
2025-08-12 14:55:03 +0200

    feat(OG-0301): Add general details in trips details

--- src/app/pages/my-trip-details/my-trip-details.page.ts
+++ src/app/pages/my-trip-details/my-trip-details.page.ts
@@ -15,7 +15,7 @@ import { Activity } from 'src/app/models/activity.model';
 export class MyTripDetailsPage implements OnInit {
 
   tripId!: number;
-  trip: Voyage[] = []
+  trip!: Voyage;
   city: string = '';
   transports: Transport[] = [];
   logements: Logement[] = [];
@@ -41,28 +41,7 @@ export class MyTripDetailsPage implements OnInit {
       },
       error: (err) => console.error('Erreur voyage:', err)
     });
-
-
-    // this.trip = tripDetails[this.tripId];
-
-    // if (this.trip) {
-    //   this.transports = this.trip.transports || [];
-    //   this.logements = this.trip.logements || [];
-    //   this.activites = this.trip.activites || [];
-    // }
-
-    // this.city = this.getCityNameById(this.tripId);
   }
-  // getCityNameById(id: number): string {
-  //   switch (id) {
-  //     case 1: return 'Paris';
-  //     case 2: return 'Seoul';
-  //     case 3: return 'New York';
-  //     case 4: return 'Tokyo';
-  //     case 5: return 'Rome';
-  //     default: return 'Inconnu';
-  //   }
-  // }
 
   selectTab(tab: any) {
     this.selectedTab = tab;
